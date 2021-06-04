728d99ef30197738bd3be07e897da6e691f53bc7
Chase Donovan <chase.donovan@aol.com>
2021-06-03 18:46:30 -0700

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import { BrowserRouter, BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+
+function Home(props){
+  return(
+    <div>Home</div>
+  )
+}
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +23,9 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data)
+          setMovieList(response.data)
+          setSaved(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -28,7 +40,11 @@ export default function App () {
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList list={[movieList]} />
+        <Link to ="/">Home</Link>
+        <Link to ='/movies/:id'>About</Link>
+        <Route  exact path="/" render={() => <MovieList movies={movieList}/>} />
+        <Route path="/about" component ={<Movie props={saved.id}/>} />
 
       <div>Replace this Div with your Routes</div>
     </div>
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,8 +4,8 @@ export default function SavedList(props) {
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie, index) => (
+        <span className="saved-movie">{(index, movie.title)}</span>
       ))}
       <div className="home-button">Home</div>
     </div>
